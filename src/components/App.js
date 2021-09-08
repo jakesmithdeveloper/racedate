1376ec5fcbbc5dadba4d051448cfeb5c74ddd035
Jake Smith <jake@Jakes-MacBook-Pro.local>
2021-09-07 22:24:50 -0400

    added react helmet for body attribute screen height

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Helmet } from "react-helmet";
 import { Router, Route, Switch } from "react-router-dom";
 
 import ComputationPage from "./ComputationPage";
@@ -8,6 +9,9 @@ import history from "../history";
 const App = () => {
   return (
     <div>
+      <Helmet>
+        <body className="h-screen bg-background-green"></body>
+      </Helmet>
       <Router history={history}>
         <Switch>
           <Route path="/" exact component={ComputationPage} />
